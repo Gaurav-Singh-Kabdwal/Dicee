4fe2d58aec0327281b70656df2ce73601b88e1ea
Gaurav Singh Kabdwal <gskabdwal@gmail.com>
2023-05-08 21:05:15 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:flutter/material.dart';
+
+import 'package:dicee/gradient_container.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          Color.fromARGB(255, 33, 5, 109),
+          Color.fromARGB(255, 68, 21, 149),
+        ),
+      ),
+    ),
+  );
+}
